6a8d5a2b0fb826bc13a733c60d8b3a50860b61af
Manolache Gabriela <manolachegabriela23@gmail.com>
2025-05-29 14:55:24 +0300

    added btw_number reset and visibility based on has_btw

--- resources/js/components/EditClientModal.vue
+++ resources/js/components/EditClientModal.vue
@@ -86,17 +86,20 @@ const submit = () => {
                                 type="checkbox"
                                 v-model="form.has_btw"
                                 class="sr-only peer"
+                                :checked="form.has_btw"
                             />
                             <div
                                 class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary"
                             ></div>
-                            <span
-                                class="text-sm font-medium text-gray-900 dark:text-gray-300"
+                            <span class="text-sm font-medium text-gray-900"
                                 >BTW</span
                             >
                         </label>
                     </div>
-                    <div class="flex items-center justify-between w-full">
+                    <div
+                        v-show="form.has_btw"
+                        class="flex items-center justify-between w-full"
+                    >
                         <label for="client_bwt">Klant BWT nummer:</label>
                         <input
                             type="text"
