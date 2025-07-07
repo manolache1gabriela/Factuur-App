a7fc0244b879e5205c4abe7eeaa5327c30feffcc
Claudiu Popa <claudiup340@gmail.com>
2025-07-07 18:32:28 +0300

    Fix some small bugs

--- resources/js/components/Layout.vue
+++ resources/js/components/Layout.vue
@@ -202,6 +202,10 @@ const deleteClient = (clientId) => {
     });
 };
 
+let canDeleteRows = computed(() => {
+    return rowsData.value.length > 1;
+});
+
 onMounted(() => {
     form.value.currentClient = usePage().props.client_id || 0;
 });
@@ -436,6 +440,8 @@ onMounted(() => {
                             :key="index"
                             :index="index"
                             :row="row"
+                            :can-delete="canDeleteRows"
+                            @delete-row="rowsData.splice(index, 1)"
                         />
                         <div
                             class="w-full h-12 rounded-lg bg-stripes bg-cover bg-no-repeat opacity-30"
